b027bbb8378116fae2058fb96adce767ab0d4059
vijay singh <111970127+VijaySinghME@users.noreply.github.com>
2023-07-25 12:50:18 +0530

    Add files via upload

---
+++ Project/studentform/qualifications/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import Qualification
+from .serializers import QualificationSerializer
+
+class QualificationViewSet(viewsets.ModelViewSet):
+    queryset = Qualification.objects.all()
+    serializer_class = QualificationSerializer
